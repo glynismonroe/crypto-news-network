fe2dc8695e833761df895216109fbedeb079771b
Glynis Monroe <glynis.d.monroe@gmail.com>
2018-08-02 18:38:56 +0000

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,23 @@
+class HomeController < ApplicationController
+  def index
+    require 'net/http'
+    require 'json'
+    
+    # Grab News Data
+    @url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @news = JSON.parse(@response)
+    
+    # Grab Price Data
+    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,ECH,EOS,LTC,ADA,XLM,MIOTA,USDT&tsyms=USD'
+    @prices_uri = URI(@prices_url)
+    @prices_response = Net::HTTP.get(@prices_uri)
+    @prices = JSON.parse(@prices_response)
+    
+  end
+  
+  def prices
+  end
+  
+end
